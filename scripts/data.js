149b33fab35eda4232eb4035cc1a7428f67df479
Philipp Parzer <phil0898.pp@gmail.com>
2021-10-03 02:02:28 +0200

    mobile layout changed

--- scripts/data.js
+++ scripts/data.js
@@ -3,21 +3,21 @@ const PROJECT_DESC = {
         title: "BUOY",
         en: " a desktop productivity app",
         de: " Produktivität für den Desktop",
-        ru: " помогает сосредоточится на работе за компьютером"
+        ru: " помогает сосредоточиться при работе за компьютером"
     },
 
     reimhard: {
         title: "REIMHARD",
         en: " a rap battle discord bot",
         de: " ein Discord-Bot für Rap-Battles",
-        ru: " бот-организатор рэп-баттлов на Дискорде"
+        ru: " бот-организатор рэп-баттлов на дискорде"
     },
 
     wip1: {
         title: "TBA",
         en: " a new way to show off digital art",
         de: " ein Weg, digitale Kunst greifbar zu machen",
-        ru: " способ похвастаться цифровами искусствами"
+        ru: " способ похвастаться цифровыми искусствами"
     },
 
     wip2: {
@@ -32,5 +32,5 @@ const PROJECT_DESC = {
 const I_AM = {
     en: ["eager to learn.", "a developer.", "a linguist.", "a translator.", "a creator.", "overthinking.", "a media guy.", "a master of all trades.", "a jack of none.", "23.", "in a hurry.", "a designer."],
     de: ["wissbegierig.", "Entwickler.", "Linguist.", "Übersetzer.", "Creator.", "am Grübeln.", "was mit Medien.", "Allrounder.", "23.", "in Eile.", "Designer."],
-    ru: ["— любопытный.", "— разработчик", "— лингвист.","— переводчик.","— создатель.","— мечтатель.","— работничек СМИ.","— мастер не на все руки.","— 23-летний студент.","— в спешке.", "— дизайнер."]
+    ru: ["— любопытный.", "— разработчик.", "— лингвист.","— переводчик.","— создатель.","— мечтатель.","— работничек СМИ.","— мастер не на все руки.","— 23-летний студент.","— в спешке.", "— дизайнер."]
 }
