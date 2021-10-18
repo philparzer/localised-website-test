e244302842c8d8ce8656a0a5144cc6e87ef796a5
Philipp Parzer <phil0898.pp@gmail.com>
2021-10-18 21:13:17 +0200

    v0.9, added next project button, removed hard-coded projects, projects 100% extracted from data.js, num of projects doesnt matter, added styling for WIP projects

--- scripts/data.js
+++ scripts/data.js
@@ -1,36 +1,69 @@
 const VERSION = {
-    en: "version 0.8",
-    de: "Version 0.8",
-    ru: "Версия 0.8"
+    en: "version 0.9",
+    de: "Version 0.9",
+    ru: "Версия 0.9"
 };
 
-const PROJECT_DESC = {
+const PROJECT_DESC = { //in descending display order
+
+    zebras: {
+        title: "ZEBRA-PIONEERS",
+        en: " a NFT drop featuring 6 animated zebras",
+        de: " NFT-Kollektion mit 6 animierten Zebras",
+        ru: " коллекция аннимированных зебр-NFT-токенов",
+        hrefLink: "https://opensea.io/collection/zebra-pioneers",
+        srcImg: "./img/zebra-pioneers.png",
+        WIP: false
+    },
+
     buoy: {
         title: "BUOY",
         en: " a desktop productivity app",
         de: " Produktivität für den Desktop",
-        ru: " помогает сосредоточиться при работе за компьютером"
+        ru: " помогает сосредоточиться при работе за компьютером",
+        hrefLink: "https://buoy-productivity.com/",
+        srcImg: "./img/buoy.png",
+        WIP: false
     },
 
     reimhard: {
         title: "REIMHARD",
         en: " a rap battle discord bot",
         de: " ein Discord-Bot für Rap-Battles",
-        ru: " бот-организатор рэп-баттлов на дискорде"
+        ru: " бот-организатор рэп-баттлов на дискорде",
+        hrefLink: "https://github.com/philparzer/reimhard",
+        srcImg: "./img/reimhard.png",
+        WIP: false
+    },
+
+    spi: {
+        title: "SPI",
+        en: " a translation of Victor Pelevin's short story 'Spi' into German",
+        de: ' Übersetzung von Wiktor Pelewins Kurzgeschichte "Spi" ins Deutsche',
+        ru: " перевод рассказа Виктора Пелевина «Спи» на немецкий",
+        hrefLink: "./pdfs/SPI_Probeübersetzung_PhilippParzer.pdf",
+        srcImg: "./img/spi.png",
+        WIP: false
     },
 
     wip1: {
         title: "TBA",
         en: " a new way to show off digital art",
         de: " ein Weg, digitale Kunst greifbar zu machen",
-        ru: " способ похвастаться цифровыми искусствами"
+        ru: " способ похвастаться цифровыми искусствами",
+        hrefLink: "#",
+        srcImg: "./img/wip1.png",
+        WIP: true
     },
 
     wip2: {
         title: "TBA",
         en: " a 3D rogue-like set in eastern europe",
         de: " 3D Rogue-like mit Setting in Osteuropa",
-        ru: " 3D игра «рогалик», события которой происходят в бывшом советском государстве"
+        ru: " 3D игра «рогалик», события которой происходят в бывшом советском государстве",
+        hrefLink: "#",
+        srcImg: "./img/wip2.png",
+        WIP: true
     }
 }
 
--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,19 @@ let DESCTIPTION_TEXT = document.getElementById("description-text");
 let DESCRIPTION_BODY = document.getElementById("description-body");
 let DESCRIPTION_BODY_MOBILE = document.getElementById("mobile-description-body");
 let DESCRIPTION_TEXT_MOBILE = document.getElementById("mobile-description-text");
+let MOBILE_CONTAINER = document.getElementById("mobile-proj");
+let mobileNextProject = 0;
 let HEADLINE_0 = document.getElementById("headline-0");
 let HEADLINE_1 = document.getElementById("headline-1");
 let HEADLINE_2 = document.getElementById("headline-2");
 let HEADLINE_3 = document.getElementById("headline-3");
-var mobileDescriptionIndex = 0;
 let highlighted;
 let notHoveringSince = false; 
+
+const PROJECT_CONTAINER_1 = document.getElementById("proj-container-1");
+const PROJECT_CONTAINER_2 = document.getElementById("proj-container-2");
+const PROJECT_CONTAINER_3 = document.getElementById("proj-container-3");
+const PROJECT_CONTAINER_4 = document.getElementById("proj-container-4");
 
 try{document.getElementById("mobile-version").textContent = VERSION[document.documentElement.lang];}
 catch{}
@@ -98,36 +104,147 @@ const fadeOut = () => {
     }, 5000)
 }
 
-const mobileBtn = (btn) => {
-
-    var lookUpArr = Object.getOwnPropertyNames(PROJECT_DESC);
-    var projectObjLen = lookUpArr.length;
-
-    if (btn === "next"){
-
-        if (mobileDescriptionIndex < (projectObjLen -1)){
-            mobileDescriptionIndex++;
-        }
-
-        else {mobileDescriptionIndex = 0}
-    }
-    else {
-        if (mobileDescriptionIndex === 0){
-            mobileDescriptionIndex = projectObjLen -1;
-        }
-        else {
-            mobileDescriptionIndex--;
-        }
-
-    }
+const nextProject = (command) => {
     
-    var lookUpStr = lookUpArr[mobileDescriptionIndex]
+    let projectProperties = Object.getOwnPropertyNames(PROJECT_DESC);
 
-    DESCRIPTION_TEXT_MOBILE.textContent = PROJECT_DESC[lookUpStr].title;
-    DESCRIPTION_BODY_MOBILE.textContent = PROJECT_DESC[lookUpStr][document.documentElement.lang];
+    let projectContainers = [PROJECT_CONTAINER_1, PROJECT_CONTAINER_2, PROJECT_CONTAINER_3, PROJECT_CONTAINER_4]
 
 
+    
+    if (command === 'desktop') {
+        for (let m = 0; m < projectContainers.length; m++){
+            let nextProject = projectProperties.indexOf(projectContainers[m].children[0].children[0].id) -1
+            if (nextProject < 0) {nextProject = projectProperties.length-1}
+
+            projectContainers[m].children[0].children[0].id = projectProperties[nextProject];
+
+            if (document.documentElement.lang === 'en') {projectContainers[m].children[0].children[0].setAttribute("src", PROJECT_DESC[projectProperties[nextProject]].srcImg)}
+            else {projectContainers[m].children[0].children[0].setAttribute("src", `.${ PROJECT_DESC[projectProperties[nextProject]].srcImg}`)}
+
+            if (PROJECT_DESC[projectProperties[nextProject]].WIP) {projectContainers[m].children[0].removeAttribute("href"); projectContainers[m].children[0].removeAttribute("target"); projectContainers[m].children[0].className = "wip";}
+            else {projectContainers[m].children[0].setAttribute("target", "_blank"); projectContainers[m].children[0].className = "";}
+
+            if (PROJECT_DESC[projectProperties[nextProject]].hrefLink.startsWith("./") && document.documentElement.lang !== 'en'){projectContainers[m].children[0].setAttribute("href", `.${PROJECT_DESC[projectProperties[nextProject]].hrefLink}`)}
+            else {projectContainers[m].children[0].setAttribute("href", `${PROJECT_DESC[projectProperties[nextProject]].hrefLink}`)}
+        }
+    }
+
+    else if (command === 'mobile' || command === "prevMobile"){
+
+        if (command === 'mobile') {
+            mobileNextProject++
+            if (mobileNextProject > projectProperties.length-1) {mobileNextProject = 0}
+        }
+
+        else if (command === 'prevMobile')
+        {
+            mobileNextProject--
+            if (mobileNextProject < 0) {mobileNextProject = projectProperties.length-1}
+        }
+
+        DESCRIPTION_TEXT_MOBILE.textContent = PROJECT_DESC[projectProperties[mobileNextProject]].title;
+        DESCRIPTION_BODY_MOBILE.textContent = PROJECT_DESC[projectProperties[mobileNextProject]][document.documentElement.lang];
+
+        if (document.documentElement.lang !== 'en'){MOBILE_CONTAINER.children[0].children[0].setAttribute("src", `.${PROJECT_DESC[projectProperties[mobileNextProject]].srcImg}`);} 
+        else {MOBILE_CONTAINER.children[0].children[0].setAttribute("src", PROJECT_DESC[projectProperties[mobileNextProject]].srcImg);}
+
+        if (PROJECT_DESC[projectProperties[mobileNextProject]].WIP) {MOBILE_CONTAINER.children[0].removeAttribute("href"); MOBILE_CONTAINER.children[0].removeAttribute("target"); MOBILE_CONTAINER.children[0].className = "wip";}
+        else {MOBILE_CONTAINER.children[0].setAttribute("target", "_blank"); MOBILE_CONTAINER.children[0].className = ""}
+
+        if (PROJECT_DESC[projectProperties[mobileNextProject]].hrefLink.startsWith('./') && document.documentElement.lang !== 'en') {MOBILE_CONTAINER.children[0].setAttribute("href", `.${PROJECT_DESC[projectProperties[mobileNextProject]].hrefLink}`);}
+        else {MOBILE_CONTAINER.children[0].setAttribute("href", PROJECT_DESC[projectProperties[mobileNextProject]].hrefLink);}
+    }
+
+    else if (command === 'initiliazeMobile'){
+        DESCRIPTION_TEXT_MOBILE.textContent = PROJECT_DESC[projectProperties[mobileNextProject]].title;
+        DESCRIPTION_BODY_MOBILE.textContent = PROJECT_DESC[projectProperties[mobileNextProject]][document.documentElement.lang];
+
+        if (document.documentElement.lang !== 'en'){MOBILE_CONTAINER.children[0].children[0].setAttribute("src", `.${PROJECT_DESC[projectProperties[mobileNextProject]].srcImg}`);} 
+        else {MOBILE_CONTAINER.children[0].children[0].setAttribute("src", PROJECT_DESC[projectProperties[mobileNextProject]].srcImg);}
+
+        if (PROJECT_DESC[projectProperties[mobileNextProject]].WIP) {MOBILE_CONTAINER.children[0].removeAttribute("href"); MOBILE_CONTAINER.children[0].removeAttribute("target"); MOBILE_CONTAINER.children[0].className = "wip"}
+        else {MOBILE_CONTAINER.children[0].setAttribute("target", "_blank"); MOBILE_CONTAINER.children[0].className = ""}
+
+        if (PROJECT_DESC[projectProperties[mobileNextProject]].hrefLink.startsWith('./') && document.documentElement.lang !== 'en') {MOBILE_CONTAINER.children[0].setAttribute("href", `.${PROJECT_DESC[projectProperties[mobileNextProject]].hrefLink}`);}
+        else {MOBILE_CONTAINER.children[0].setAttribute("href", PROJECT_DESC[projectProperties[mobileNextProject]].hrefLink);}
+    }
+
+    else if (command === 'initializeDesktop') {
+        for (let o = 0; o < projectContainers.length ; o++){
+            projectContainers[o].children[0].children[0].id = projectProperties[o];
+
+            if (document.documentElement.lang !== 'en') {projectContainers[o].children[0].children[0].setAttribute("src", `.${PROJECT_DESC[projectProperties[o]].srcImg}`)}
+            else {projectContainers[o].children[0].children[0].setAttribute("src", `${PROJECT_DESC[projectProperties[o]].srcImg}`)}
+
+            if (PROJECT_DESC[projectProperties[o]].WIP) {projectContainers[o].children[0].removeAttribute("href"); projectContainers[o].children[0].removeAttribute("target"); projectContainers[o].children[0].className = "wip"}
+            else {projectContainers[o].children[0].setAttribute("target", "_blank"); projectContainers[o].children[0].className = ""}
+
+            if (PROJECT_DESC[projectProperties[o]].hrefLink.startsWith("./") && document.documentElement.lang !== 'en') {projectContainers[o].children[0].setAttribute("href", `.${PROJECT_DESC[projectProperties[o]].hrefLink}`)}
+            else {projectContainers[o].children[0].setAttribute("href", `${PROJECT_DESC[projectProperties[o]].hrefLink}`)}
+        }
+    }
 }
 
+//initialize mobile project
+nextProject('initiliazeMobile');
+nextProject('initializeDesktop');
 
 setTimeout(() => {updateHeadline(document.documentElement.lang)},1000)
+
+
+
+
+
+console.log(`\`
+MMMMMMMMMd\/                                                                                         
+MMMMMMMMMMMd\/                                                                                       
+MMMMMMMMMMMMMd\/\`                                                                                    
+MMMMMMMMMMMMMMMd:                                                                                   
+MMMMMMMMMMMMMMMMNy.                                                                                 
+MMMMMMMMMMMMMMMMMMm-                                                                                
+MMMMMMMMMMMMMMMMMMMN-                                                                               
+MMMMMMMMMMMMMMMMMMMMd                                                                               
+MMMMMMMMMMMMMMMMMMMMM-                          \/s:        .:\`                                      
+NMMMMMMMMMMMMMMMMMMMN.                        \`sMNNs\`\`\`  \`sNMd:                                     
+.\/smMMMMMMMMMMMMMMMN\/                        .dMh\/hMmhhyymNyomMs\`                                   
+    :hMMMMMMMMMMMmo\`                        \/NMs---+oossyy\/--:hMd.                                  
+     +MMMMMMMMNy:                          +MN\/----------------sMN-                                 
+   :mMMMMMMMd\/\`                           +MN-.\`      \`.\`\`\`\`\`\`\`\`\/MN-                                
+  :MMMMMMMN\/                             -MNhNMNo   .dMMd-       \/MN-                               
+  -NMMMMMMo                             \`mM+hMMMy   -NMMN:        +Mm\`                              
+   -smMMMMm+.                           +Mm\`\`:\/:\`    .\/\/.         \`yMh                              
+     \`:sdNMMNmyo\/-.                     mMo----............\`\`\`\`\`\`\`\`.mM+                             
+        \`.:ohmMMMNmds:\`                .MM:--------:+\/--------------+MN.                            
+             .-+hNMMMMd-               \/Mm---------oMd----\/\/---------hMy                            
+                 -yMMMM\/               +Md---------yMmyyyymh---------\/MM-                           
+                   dMMs                +Md\`\`\`\`\`\`...dMy\/\/\/\/\/:----------hMy                           
+                  \`dN+                 :MN\/\/::--...NM:            \`\`\`\`-MN\`                          
+                 .yy.                  \`yhddmMMMNNNNN.                 mM+                          
+               \/sNdoooooo\`                  \`mM\/\`..-.                  oMh                          
+               MmMd\/\/\/\/yMhyyyyyyhhhhhhhhhhhhdMm\`\`\`\`                    -MN                          
+               mNNNmmmmNMNssooo++ossssssso++NMo---------------..........MM-                         
+                \`\`\`\`\`\`\`+Md-.     .-------- :MN:-------------------------mM\/                         
+                       sMNhhssoo+ooo+++\/\/:.hMy-------------------:o\/----hMs                         
+                       .\/+oossyyhhhddddmmmNMM:-------------------oMd----sMd                         
+                                 \`\`\`\`\`\`...yMy\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`..\/MN....\/MN                         
+                                         \`NM:                    .MM\`   \`MM.                        
+                                         :MN\`                    \`MM-    mM\/                        
+                                         yMy                      mM\/    yMs                        
+                                         mM+..........\`\`\`\`\`\`\`\`\`\`\`\`hMo\`\`\`\`oMh                        
+                                        .MM:----------------------yMh----+MN                        
+                                        \/MN-----------------------sMm----:MM.                       
+                                        oMd-----------------------+MN-----NM\/                       
+                                        yMs............-----------\/MMhhhhhNMo                       
+                                        dM+                       \`+sNMhooo+.                       
+                                        mM\/                          mM\/                            
+                                        MM-        .hddddddy\`        mM\/                            
+                                       \`MM-\`\`\`\`\`\`\` \/MN\/\/\/oMM\`        dM\/                            
+                                       .MM:--------oMm   -MM.........mM\/                            
+                                       -MM---------oMd   -MM---------mM+                            
+                                       :MN:::::::::sMd   :MN:::::::::mM+                            
+                                       :MMddddmmmmmNMy   :MMmmmddddddNM\/                            
+                                       \`\/\/\/\/\/\/\/\/:::::.   \`::::\/\/\/\/\/\/\/\/\/\`
+
+`
+)
